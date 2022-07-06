78b0892d39e209d82fd1986a3a12919705b0bb9f
FrancescoSanti96 <francescosanti123@gmail.com>
2022-07-06 17:30:12 +0200

    pass checkbox chechek value to the other page

--- src/pages/phrase.js
+++ src/pages/phrase.js
@@ -4,7 +4,7 @@ import { Link } from "gatsby"
 export default function Component ({location}) {
   return (
     <div>
-      <h1>Hi from the second page, {location.state}</h1>
+      <h1>Hi from the second page, {location.state.test}</h1>
       <Link to="/">HOME</Link>
     </div>
     
--- src/pages/word.js
+++ src/pages/word.js
@@ -5,10 +5,11 @@ import { useState, useEffect } from "react";
 const WordPage = ({ serverData }) => {
   // per prelevare i valori contenuti nella checkbox selezionate
   const [words, setWords] = useState([]);
+  let prova = "ciao"
   //prelevare la props della chiamata api
   const { title } = serverData;
 useEffect(() => {
-  console.log(words)
+  console.log(Object.values(words))
 }, [words])
 
   return (
@@ -20,17 +21,18 @@ useEffect(() => {
         <legend>
           <h1>Seleziona le parole per comporrere la frase:</h1>
         </legend>
-
+        {/* creo una map per ogni valore prelevato con l'api e creou una checkbox */}
         {title.map((item, i) => (
           <div key={i}>
             <input type="checkbox" id={item.title} name={item.title} 
+            // monitoro quando vine seleziona e deselezionata per aggiugnere o rimuovere il valore
             onChange={(e) => {
                 // add to list
                 if (e.target.checked) {
                   setWords([
                     ...words,
                     {
-                      id: item.id,
+                      id: item.title,
                     }
                   ]);
                 } else {
@@ -50,7 +52,14 @@ useEffect(() => {
       {/* TODO */}
       {/* solo se è stata selezionato almeno 1 valore aggiugi il bottone per andate avanti */}
       {/* state permette di passare come props dei valori e poi richiamarli nella pagina con il location */}
-      <Link to="/phrase" > 
+      <button
+      onClick={()=> {
+        // console.log(JSON.stringify(words))
+        setWords(JSON.stringify(words))
+        // localStorage.setItem("val", JSON.stringify(words));
+      }}
+      >prova</button>
+      <Link to="/phrase" state={{ test: words }}> 
         Phrase
       </Link> 
     </>
